5ca85023b80ac655054b27e308a765afd4e19cb0
DominikSzkotland <dszlencka@outlok.com>
2023-09-20 17:20:33 +0200

    Kolejne zadanie

--- Notatka.java
+++ Notatka.java
@@ -4,4 +4,21 @@ public class Notatka {
     protected String tytul;
     protected String tresc;
 
+    public Notatka(int identyfikator, String tytul, String tresc) {
+        notatkaLicznik++;
+        identyfikator = notatkaLicznik;
+        this.tytul = tytul;
+        this.tresc = tresc;
+    }
+    public void  wyswietlTytulTresc()
+    {
+        System.out.println("tytol "+tytul+" tresc "+tresc);
+
+    }
+    public void
+    {
+        System.out.println(tytul+", "+tresc+", "+", "+notatkaLicznik);
+
+    }
 }
+
